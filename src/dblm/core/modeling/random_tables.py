d8a93cde56c7df842bb0039fe458d2fcf56a7109
Brian Lu <brian.luzc@gmail.com>
2023-08-07 16:35:28 -0700

    Rename and add some random inits to LogProbTable

--- src/dblm/core/modeling/random_tables.py
+++ src/dblm/core/modeling/random_tables.py
@@ -7,21 +7,36 @@ import enum
 
 
 class TensorInitializer(enum.Enum):
-    UNIFORM = enum.auto()
+    CONSTANT = enum.auto()
     GAUSSIAN = enum.auto()
+    UNIFORM = enum.auto()
 
 
 class LogProbTable(nn.Module, pgm.ProbabilityTable):
 
-    def __init__(self, size: int, intializer: TensorInitializer, requires_grad:bool=True) -> None:
+    def __init__(self, size: int, initializer: TensorInitializer, requires_grad:bool=True) -> None:
         super().__init__()
         # Store as ndarray
-        if intializer is TensorInitializer.UNIFORM:
-            self.logprob = nn.Parameter(torch.zeros(size), requires_grad=requires_grad)
+        if initializer is TensorInitializer.CONSTANT:
+            self.logits = nn.Parameter(torch.zeros(size), requires_grad=requires_grad)
+        elif initializer is TensorInitializer.UNIFORM:
+            self.logits = nn.Parameter(torch.rand(size), requires_grad=requires_grad)
         else:
             raise NotImplementedError()
 
     def probability_table(self):
-        original_size = self.logprob.size()
-        flattened_logprob = self.logprob.view(-1)
-        return flattened_logprob.softmax(dim=-1).view(original_size)
+        original_size = self.logits.size()
+        flattened_logits = self.logits.view(-1)
+        return flattened_logits.softmax(dim=-1).view(original_size)
+
+
+    def log_probability_table(self):
+        original_size = self.logits.size()
+        flattened_logits = self.logits.view(-1)
+        return flattened_logits.log_softmax(dim=-1).view(original_size)
+
+    def likelihood_function(self, assignment):
+        return self.probability_table()[assignment]
+
+    def log_likelihood_function(self, assignment):
+        return self.log_probability_table()[assignment]
